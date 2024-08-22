e5cd826099a21571dde209e921e543f1bfc4608b
Henzo Alejandro Arrué Muñoz <henzo.profe.mate@gmail.com>
2024-08-21 20:12:22 -0400

    21082024-2012

--- porcentajes_logro.py
+++ porcentajes_logro.py
@@ -5,10 +5,10 @@ mbueno = float(input("Ingrese muy buenos: "))
 
 total = ins + suf + bueno + mbueno
 
-pins = round((100 * ins / total),1)
-psuf = round((100 * suf / total),1)
-pbueno = round((100 * bueno / total),1)
-pmbueno = round((100 * mbueno / total),1)
+pins = round((100 * ins / total), 1)
+psuf = round((100 * suf / total), 1)
+pbueno = round((100 * bueno / total), 1)
+pmbueno = round((100 * mbueno / total), 1)
 
 print(f"El total de alumnos es {total}")
 print(f"I={pins}%\nS={psuf}%\nB={pbueno}\nMB={pmbueno}")
---
+++ sel2x2.py
@@ -0,0 +1,7 @@
+print("Ordene las ecuaciones y luego ingrese los coeficientes de forma ordenada")
+a1=float(input("a1="))
+b1=float(input("b1="))
+c1=float(input("c1="))
+a2=float(input("a2="))
+b2=float(input("b2="))
+c2=float(input("c2="))
