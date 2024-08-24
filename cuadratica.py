c87836cde0b5dd7d1fc2d7d38f36e96f639cafe8
Henzo Alejandro Arrué Muñoz <henzo.profe.mate@gmail.com>
2024-08-24 16:20:54 -0400

    24082024-1620

--- cuadratica.py
+++ cuadratica.py
@@ -12,19 +12,16 @@ while True:
     disc = b * b - 4 * a * c
     print("El discriminante es:", disc)
     if disc < 0:
-        print("No existe solución real")
+        print("No existe solución real\nExisten dos soluciones conplejas")
+        xc1 =(-b - (-disc)**0.5) / (2 * a)
+        xc2 =(-b + (-disc)**0.5) / (2 * a)
+        print("Las soluciones son",xc1," y ",xc2)
     elif disc == 0:
         print("Existe única solución real, x=", (-b / (2 * a)))
     else:
-        print(
-            "Existen dos soluciones reales:\nx1=",
-            (-b - disc**0.5) / (2 * a),
-            " y x2=",
-            (-b + disc**0.5) / (2 * a),
+        print("Existen dos soluciones reales:\nx1=", (-b - disc**0.5) / (2 * a)," y x2=",(-b + disc**0.5) / (2 * a),
         )
-    opcion = float(
-        input("Presione 1 para salir\tPresione 2 para resolver otra ecuación\nOpcion:")
-    )
+    opcion = float(input("Presione 1 para salir\tPresione 2 para resolver otra ecuación\nOpcion:"))
     if opcion == 1:
         break
 print("Gracias por usar el programa")
